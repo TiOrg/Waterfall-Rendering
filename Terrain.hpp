7eb902dd660b555d6a2f97cd94627b61ad62a9d0
张子岳 <simon0628@163.com>
2018-12-06 14:41:39 +0800

    融合天空盒&地形导入
    天空盒位置不对，模型还要改

---
+++ Terrain.hpp
@@ -0,0 +1,13 @@
+//
+//  Terrain.hpp
+//  waterfall
+//
+//  Created by simon on 2018/12/6.
+//  Copyright © 2018 simon. All rights reserved.
+//
+
+#ifndef Terrain_h
+#define Terrain_h
+
+
+#endif /* Terrain_h */
--- main.cpp
+++ main.cpp
@@ -15,10 +15,13 @@ using namespace glm;
 
 //#include "common/shader.hpp"
 #include "tools/texture.hpp"
+#include "tools/objloader.hpp"
 //#include "common/controls.hpp"
 #include "Particle.hpp"
 #include "Camera.hpp"
 #include "Shader.hpp"
+#include "Skybox.hpp"
+#include "Terrain.hpp"
 #pragma comment(lib, "glfw3.lib")
 
 
@@ -35,7 +38,8 @@ const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
 
 // camera: initiliazed by start location
-Camera camera(glm::vec3(0.0f, 0.0f, 4.0f));
+vec3 sceneCenter(80.f, 0.f, 180.f);
+Camera camera(sceneCenter+vec3(0.f, 0.f, 15.f));
 float lastX = (float)SCR_WIDTH / 2.0;
 float lastY = (float)SCR_HEIGHT / 2.0;
 bool firstMouse = true;
@@ -99,32 +103,63 @@ int main( void )
 	// Accept fragment if it closer to the camera than the former one
 	glDepthFunc(GL_LESS);
 
+    
+    
     //===============================================================================================
+    // prepare skybox
     //===============================================================================================
+    // skybox VAO
+    GLuint skyboxVertexArray;
+    glGenVertexArrays(1, &skyboxVertexArray);
+    glBindVertexArray(skyboxVertexArray);
+
+    Shader skyboxShader("shader/SkyboxShader.vs", "shader/SkyboxShader.fs");
+
+    GLuint skybox_vertex_buffer;
+    glGenBuffers(1, &skybox_vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, skybox_vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    
+
+    std::vector<std::string> faces
+    {
+        "material/right.jpg",
+        "material/left.jpg",
+        "material/top.jpg",
+        "material/bottom.jpg",
+        "material/front.jpg",
+        "material/back.jpg"
+    };
+    GLuint cubemapTexture = loadCubemap(faces);
+
+    
+    //===============================================================================================
+    // prepare particle
     //===============================================================================================
     
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
+	GLuint particleVertexArray;
+	glGenVertexArrays(1, &particleVertexArray);
+	glBindVertexArray(particleVertexArray);
 
 	// Create and compile our GLSL program from the shaders
-    Shader particleShader( "ParticleShader.vs", "ParticleShader.fs" );
+    Shader particleShader( "shader/ParticleShader.vs", "shader/ParticleShader.fs" );
 
-	static GLfloat* g_particule_position_size_data = new GLfloat[MaxParticles * 4];
-	static GLubyte* g_particule_color_data         = new GLubyte[MaxParticles * 4];
+	static GLfloat* g_particle_position_data = new GLfloat[MaxParticles * 4];
+	static GLubyte* g_particle_color_data    = new GLubyte[MaxParticles * 4];
 
     // init particles
-	for(int i=0; i<MaxParticles; i++){
+	for(int i=0; i<MaxParticles; i++)
+    {
 		ParticlesContainer[i].life = -1.0f;
 		ParticlesContainer[i].cameradistance = -1.0f;
 	}
 
-	GLuint particleTexture = loadDDS("particle.DDS");
+	GLuint particleTexture = loadDDS("material/particle.DDS");
 
     // init VBO
 	// The VBO containing the 4 vertices of the particles.
 	// Thanks to instancing, they will be shared by all particles.
-	static const GLfloat g_vertex_buffer_data[] = { 
+	static const GLfloat g_container_vertex_data[] = {
 		 -0.5f, -0.5f, 0.0f,
 		  0.5f, -0.5f, 0.0f,
 		 -0.5f,  0.5f, 0.0f,
@@ -133,7 +168,7 @@ int main( void )
 	GLuint container_vertex_buffer;
 	glGenBuffers(1, &container_vertex_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, container_vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_container_vertex_data), g_container_vertex_data, GL_STATIC_DRAW);
 
 	// The VBO containing the positions and sizes of the particles
 	GLuint particles_position_buffer;
@@ -150,11 +185,34 @@ int main( void )
 	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
 
     //===============================================================================================
+    // prepare terrain
     //===============================================================================================
-    //===============================================================================================
+    
+    GLuint terrainVertexArray;
+    glGenVertexArrays(1, &terrainVertexArray);
+    glBindVertexArray(terrainVertexArray);
+    
+    // Create and compile our GLSL program from the shaders
+    Shader terrainShader( "shader/TerrainShader.vs", "shader/TerrainShader.fs" );
+    
+    // Read our .obj file
+    std::vector<glm::vec3> vertices;
+    loadOBJ("material/waterfall-less.obj", vertices);
+    
+    // Load it into a VBO
+    GLuint terrain_vertex_buffer;
+    glGenBuffers(1, &terrain_vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, terrain_vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+    
     
     while( !glfwWindowShouldClose(window))
     {
+        
+        //===============================================================================================
+        // render init
+        //===============================================================================================
+        
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -175,19 +233,50 @@ int main( void )
 
         
         //===============================================================================================
+        // terrain render
         //===============================================================================================
+        
+        terrainShader.use();
+        // Compute the MVP matrix from keyboard and mouse input
+        
+        glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+        
+        // Send our transformation to the currently bound shader,
+        // in the "MVP" uniform
+        terrainShader.setMat4("MVP", MVP);
+        
+        
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, terrain_vertex_buffer);
+        glVertexAttribPointer(
+                              0,                  // attribute
+                              3,                  // size
+                              GL_FLOAT,           // type
+                              GL_FALSE,           // normalized?
+                              0,                  // stride
+                              (void*)0            // array buffer offset
+                              );
+        
+        // 2nd attribute buffer : UVs
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
+        glDisableVertexAttribArray(0);
+        
+        
+        //===============================================================================================
+        // particle render
         //===============================================================================================
         
         // update particles
         
-		// Generate ule each millisecond,
+		// Generate new particles each millisecond,
 		// but limit this to 16 ms (60 fps), or if you have 1 long frame (1sec),
-		// newparticles will be huge and the next frame even longer.
+		// new particles will be huge and the next frame even longer.
 		int newparticles = (int)(deltaTime*10000.0);
 		if (newparticles > (int)(0.016f*10000.0))
 			newparticles = (int)(0.016f*10000.0);
 		
-		for(int i=0; i<newparticles; i++){
+		for(int i = 0; i < newparticles; i++){
 			int particleIndex = FindUnusedParticle();
             
             // params of particles' moving are shown below
@@ -196,11 +285,11 @@ int main( void )
 			ParticlesContainer[particleIndex].life = 5.0f; // This particle will live 5 seconds.
             
             // init position
-			ParticlesContainer[particleIndex].pos = glm::vec3(0,0,-20.0f);
+			ParticlesContainer[particleIndex].pos = glm::vec3(sceneCenter+vec3(0.f, 3.5f, -15.f));
 
             // init speed direction
             float spread = 1.5f;
-            glm::vec3 maindir = glm::vec3(0.0f, 5.0f, 2.0f);
+            glm::vec3 maindir = glm::vec3(0.0f, 1.0f, 2.0f);
             glm::vec3 randomdir = glm::vec3(
 				(rand()%2000 - 1000.0f)/1000.0f,
 				(rand()%2000 - 1000.0f)/1000.0f,
@@ -209,9 +298,9 @@ int main( void )
 			ParticlesContainer[particleIndex].speed = maindir + randomdir*spread;
 
             // blue color with random alpha
-			ParticlesContainer[particleIndex].r = 0.1 * 256;
-			ParticlesContainer[particleIndex].g = 0.6 * 256;
-			ParticlesContainer[particleIndex].b = 0.6 * 256;
+			ParticlesContainer[particleIndex].r = 0.2 * 256;
+			ParticlesContainer[particleIndex].g = 0.7 * 256;
+			ParticlesContainer[particleIndex].b = 0.7 * 256 + 0.2 * (rand() % 256);
 			ParticlesContainer[particleIndex].a = (rand() % 256) / 3;
 
             // random size
@@ -223,53 +312,51 @@ int main( void )
         
 		// Simulate all particles
 		int ParticlesCount = 0;
-		for(int i=0; i<MaxParticles; i++){
-
+		for(int i=0; i<MaxParticles; i++)
+        {
 			Particle& p = ParticlesContainer[i]; // shortcut
-
-			if(p.life > 0.0f){
-
-				// Decrease life
-				p.life -= deltaTime;
-				if (p.life > 0.0f){
-
+            
+			if(p.life > 0.0f)
+            {
+				p.life -= deltaTime; // Decrease life
+				if (p.life > 0.0f)
+                {
 					// Simulate simple physics : gravity only, no collisions
-					p.speed += glm::vec3(0.0f,-9.81f, 0.0f) * (float)deltaTime * 0.5f;
+					p.speed += glm::vec3(0.0f,-9.81f, 0.0f) * (float)deltaTime;
 					p.pos += p.speed * (float)deltaTime;
 					p.cameradistance = glm::length2( p.pos - CameraPosition );
-					//ParticlesContainer[i].pos += glm::vec3(0.0f,10.0f, 0.0f) * (float)deltaTime;
 
 					// Fill the GPU buffer
-					g_particule_position_size_data[4*ParticlesCount+0] = p.pos.x;
-					g_particule_position_size_data[4*ParticlesCount+1] = p.pos.y;
-					g_particule_position_size_data[4*ParticlesCount+2] = p.pos.z;
+					g_particle_position_data[4*ParticlesCount+0] = p.pos.x;
+					g_particle_position_data[4*ParticlesCount+1] = p.pos.y;
+					g_particle_position_data[4*ParticlesCount+2] = p.pos.z;
 												   
-					g_particule_position_size_data[4*ParticlesCount+3] = p.size;
+					g_particle_position_data[4*ParticlesCount+3] = p.size;
 												   
-					g_particule_color_data[4*ParticlesCount+0] = p.r;
-					g_particule_color_data[4*ParticlesCount+1] = p.g;
-					g_particule_color_data[4*ParticlesCount+2] = p.b;
-					g_particule_color_data[4*ParticlesCount+3] = p.a;
+					g_particle_color_data[4*ParticlesCount+0] = p.r;
+					g_particle_color_data[4*ParticlesCount+1] = p.g;
+					g_particle_color_data[4*ParticlesCount+2] = p.b;
+					g_particle_color_data[4*ParticlesCount+3] = p.a;
 
-				}else{
+				}
+                else
+                {
 					// Particles that just died will be put at the end of the buffer in SortParticles();
 					p.cameradistance = -1.0f;
 				}
-
 				ParticlesCount++;
-
 			}
 		}
 		SortParticles();
 
         // main buffer draw
 		glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
-		glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
-		glBufferSubData(GL_ARRAY_BUFFER, 0, ParticlesCount * sizeof(GLfloat) * 4, g_particule_position_size_data);
+		glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, ParticlesCount * sizeof(GLfloat) * 4, g_particle_position_data);
 
 		glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
-		glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
-		glBufferSubData(GL_ARRAY_BUFFER, 0, ParticlesCount * sizeof(GLubyte) * 4, g_particule_color_data);
+		glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, ParticlesCount * sizeof(GLubyte) * 4, g_particle_color_data);
 
 
 		glEnable(GL_BLEND);
@@ -329,15 +416,42 @@ int main( void )
 		glVertexAttribDivisor(1, 1); // positions : one per quad (its center)                 -> 1
 		glVertexAttribDivisor(2, 1); // color : one per quad                                  -> 1
 
-		// Draw the particules !
+		// Draw particles
 		glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, ParticlesCount);
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
 		glDisableVertexAttribArray(2);
 
+        
+        
         //===============================================================================================
+        // skybox render
         //===============================================================================================
+        
+        
+        // draw skybox as last
+        glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+        skyboxShader.use();
+        skyboxShader.setMat4("VP", ViewProjectionMatrix);
+        // skybox cube
+        
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        skyboxShader.setInt("skybox", 0);
+        
+        
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, skybox_vertex_buffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glDisableVertexAttribArray(0);
+
+        glDepthFunc(GL_LESS); // set depth function back to default
+
+        //===============================================================================================
+        // render end
         //===============================================================================================
         
 		// Swap buffers
@@ -347,13 +461,13 @@ int main( void )
 	}
     
     // Do some cleans
-    delete[] g_particule_color_data;
-	delete[] g_particule_position_size_data;
+    delete[] g_particle_color_data;
+	delete[] g_particle_position_data;
 	// Cleanup VBO and shader
 	glDeleteBuffers(1, &particles_color_buffer);
 	glDeleteBuffers(1, &particles_position_buffer);
 	glDeleteBuffers(1, &container_vertex_buffer);
-	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteVertexArrays(1, &particleVertexArray);
     particleShader._delete();
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
@@ -369,17 +483,17 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-    else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.ProcessKeyboard(FORWARD, deltaTime);
-    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
         camera.ProcessKeyboard(BACKWARD, deltaTime);
-    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         camera.ProcessKeyboard(LEFT, deltaTime);
-    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
-    else if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
         camera.ProcessKeyboard(UPWORD, deltaTime);
-    else if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
         camera.ProcessKeyboard(DOWNWORD, deltaTime);
 }
 
